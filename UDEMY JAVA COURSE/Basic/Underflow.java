ca0e4d21e5e27a1c94045ae06a1114aa0311bc28
Ajey23 <ajaysarode2121@gmail.com>
2022-07-27 07:16:47 +0530

    new

--- UDEMY JAVA COURSE/Basic/Underflow.java
+++ UDEMY JAVA COURSE/Basic/Underflow.java
@@ -4,6 +4,7 @@ class Underflow
 {
     public static void main(String args[])
     {
+        //INTEGER
         int myMinIntValue = Integer.MIN_VALUE;
         int myMaxIntValue = Integer.MAX_VALUE;
 
@@ -11,6 +12,16 @@ class Underflow
         System.out.println("Integer Maximum Value = "+myMaxIntValue);
 
         System.out.println("Busted MAX Value = " +(myMaxIntValue + 1));     //Adding +1 to maximum integer value (OVERFLOW)
-        System.out.println("Busted MIN Value = " +(myMinIntValue - 1));     //Subtracting -1 to minimum integer valie  (UNDERFLOW)
+        System.out.println("Busted MIN Value = " +(myMinIntValue - 1));     //Subtracting -1 to minimum integer value  (UNDERFLOW)
+
+        //BYTE
+        int myMinByteValue = Byte.MIN_VALUE;
+        int myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte Minimum Value = "+myMinByteValue);
+        System.out.println("Byte Maximum Value = "+myMaxByteValue);
+
+        System.out.println("Busted MAX Value = " +(myMaxByteValue + 1));     //Adding +1 to maximum Byte value (OVERFLOW)
+        System.out.println("Busted MIN Value = " +(myMinByteValue - 1));     //Subtracting -1 to minimum Byte value  (UNDERFLOW)
     }
 }
