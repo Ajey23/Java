a3d80edfdc406de5bf092d4941eab8136de10898
Ajey23 <ajaysarode2121@gmail.com>
2022-07-28 06:53:36 +0530

    new

---
+++ UDEMY JAVA COURSE/Basic/Char.java
@@ -0,0 +1,11 @@
+class Char
+{
+    public static void main(String args[])
+    {
+        char myChar='A';
+        char myUnicode='\u0041';
+
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+    }
+}
